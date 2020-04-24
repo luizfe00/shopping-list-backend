81f637d945cdd981f8be38330dbddb2a7c4a0222
Luiz Felipe <luizzfelip00@gmail.com>
2020-04-24 15:02:39 -0300

    Full working backend

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/authMiddleware')
 
 // Item Model
 const Item = require('../../models/Item')
@@ -15,8 +16,8 @@ router.get('/', (req, res) => {
 
 // @route POST api/items
 // @desc  Create A Item
-// @acess Public
-router.post('/', (req, res) => {
+// @acess Private
+router.post('/', auth, (req, res) => {
     const newItem = new Item ({
             name: req.body.name
     })
@@ -26,8 +27,8 @@ router.post('/', (req, res) => {
 
 // @route DELETE api/items
 // @desc  Delete A Item
-// @acess Public
-router.delete('/:id', (req, res) => {
+// @acess Private
+router.delete('/:id', auth, (req, res) => {
     Item.findById(req.params.id)
       .then(item => item.remove().then(() => res.json({ success: true })))
       .catch(err => res.status(400).json({ success: false }))
--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-
-const items = require('./routes/api/items')
-
+const config = require('config')
 const app = express()
 
 // Bodyparser Middleware
-app.use(bodyParser.json())
+app.use(express.json())
 
 // DB config
-const db = require('./config/keys').mongoURL
+const db = config.get('mongoURL')
 
 // Connect to Mongo
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err))
 
 // Use Routes
-app.use('/api/items', items)
-
+app.use('/api/items', require('./routes/api/items'))
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
 
 const port = process.env.PORT || 5000
 
